0591262182ad5ea04db727eabbc90c69ab2f37ff
Harald Wilbertz <harald.wilbertz@gmx.de>
2020-11-29 18:13:49 +0100

    Recover added to PlayerCoordinatorActor.

--- GameConsole/ActorModel/Actors/PlayerCoordinatorActor.cs
+++ GameConsole/ActorModel/Actors/PlayerCoordinatorActor.cs
@@ -1,22 +1,38 @@
-using System;
 using Akka.Actor;
+using Akka.Persistence;
 using GameConsole.ActorModel.Messages;
 
 namespace GameConsole.ActorModel.Actors
 {
-    internal class PlayerCoordinatorActor : ReceiveActor
+    internal class PlayerCoordinatorActor : ReceivePersistentActor
     {
         private const int DefaultStartingHealth = 100;
 
+        public override string PersistenceId => "player-coordinator";
+
         public PlayerCoordinatorActor()
         {
-            Receive<CreatePlayerMessage>(message =>
+            Command<CreatePlayerMessage>(message =>
             {
                 DisplayHelper.WriteLine($"PlayerCoordinatorActor received CreatePlayerMessage for {message.PlayerName}");
 
+                Persist(message, createPlayerMessage =>
+                {
+                    DisplayHelper.WriteLine($"PlayerCoordinatorActor persisted a CreatePlayerMessage for {message.PlayerName}");
+
+                    Context.ActorOf(
+                        Props.Create(() =>
+                            new PlayerActor(message.PlayerName, DefaultStartingHealth)), message.PlayerName);
+                });
+            });
+
+            Recover<CreatePlayerMessage>(createPlayerMessage =>
+            {
+                DisplayHelper.WriteLine($"PlayerCoordinatorActor replaying a CreatePlayerMessage for {createPlayerMessage.PlayerName}");
+
                 Context.ActorOf(
                     Props.Create(() =>
-                                new PlayerActor(message.PlayerName, DefaultStartingHealth)), message.PlayerName);
+                        new PlayerActor(createPlayerMessage.PlayerName, DefaultStartingHealth)), createPlayerMessage.PlayerName);
             });
         }
     }
